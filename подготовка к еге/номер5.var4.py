091ee1366aca7f671fb10f6945fa26e3f8ec3cd1
daniilkrukov942-coder <faniilkrukov942@gmail.com>
2026-06-04 22:29:00 +0300

    начальный комит

---
+++ подготовка к еге/номер5.var4.py
@@ -0,0 +1,22 @@
+def perevod(chislo, sys):
+    N = chislo
+    s = ''
+    while N > 0:
+        s = str(N % sys) + s
+        N = N // sys
+    return s
+for n in range(1, 1000):
+    a = perevod(n, 3)
+    if n % 3 == 0:
+        b = '1' + a + a[-2:]
+    else:
+        sum_cifr = sum(int(digit) for digit in a)
+        x = sum_cifr * 5
+        m = perevod(x, 3)
+        b = a + m
+    r = int(b, 3)
+    print(r)
+
+
+
+
